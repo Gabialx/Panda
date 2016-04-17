e3ab575bff0f73fc92b99e5fd7d2a59aca455010
Panda <gabi.alx7.bg@gmail.com>
2016-04-17 06:19:05 +0300

    Activity for all history sub-pages

---
+++ app/src/main/java/eu/ynalbantov/android/panda/visit/vratsa/c1/c01s01i06Activity.java
@@ -0,0 +1,19 @@
+package eu.ynalbantov.android.panda.visit.vratsa.c1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.panda.myapplication.R;
+
+/**
+ * Created by Dani on 17.4.2016 г
+ */
+public class c01s01i06Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c01s01i06);
+    }
+}
+
